97576aef8e8ac19b85d4a2fe4667771bbaf39157
PaulaTam <ptam4@sfsu.edu>
2024-06-20 21:02:21 -0700

    Initialized create-react-app and TailwindCSS

---
+++ src/link_index.js
@@ -0,0 +1,7 @@
+const spark_link = "https://paulatam.github.io/spark-calculator/";
+const url_short_link = "https://paulatam.github.io/url_shortening/";
+
+export const ProjectLinks = {
+    spark_link,
+    url_short_link,
+}
